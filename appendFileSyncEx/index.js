2aeb324af9ec5feb0924f15d550b95e71aa27121
lthong <appyegde58965@gmail.com>
2021-06-13 11:54:20 +0800

    add appendFileSync ex

---
+++ appendFileSyncEx/index.js
@@ -0,0 +1,6 @@
+// CH3-9
+// 「appendFileSync(檔名, 要寫入的內容)」 可以把指定的內容加在該檔名原先的內容之後，如果該檔名不存在則會自動建立一個
+
+const fs = require('fs');
+fs.writeFileSync('note.txt', 'Hello world! ');
+fs.appendFileSync('note.txt', 'data to append');
