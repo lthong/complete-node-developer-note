3f14df277e6d5233d14bb1ab66b206f65ac27d4d
lthong <appyegde58965@gmail.com>
2021-06-25 23:16:24 +0800

    update the todoList ex

--- todoListEx/index.js
+++ todoListEx/index.js
@@ -2,19 +2,40 @@
 // 使用yargs套件，將終端輸入的結果（即要記下的待辦事項) 存在本地（與js檔案相同路徑下）的一個json檔案，功能包含「新增」、「刪除」、「讀取整個列表」
 
 const yargs = require('yargs');
-const { addTodo } = require('./utils');
+const { addTodo, removeTodo, removeAllTodo, readTodoList } = require('./utils');
 
-yargs.command(
-  'add',
-  'add a todo',
-  {
-    todo: {
-      describe: 'todo title',
-      demandOption: true,
-      type: 'string',
+yargs
+  .command(
+    'add',
+    'add a todo',
+    {
+      todo: {
+        describe: 'todo title',
+        demandOption: true,
+        type: 'string',
+      },
     },
-  },
-  ({ todo }) => {
-    addTodo(todo);
-  }
-).argv;
+    ({ todo }) => {
+      addTodo(todo);
+    }
+  )
+  .command(
+    'remove',
+    'remove a todo',
+    {
+      todo: {
+        describe: 'todo title',
+        demandOption: true,
+        type: 'string',
+      },
+    },
+    ({ todo }) => {
+      removeTodo(todo);
+    }
+  )
+  .command('read', 'read the todo list', {}, () => {
+    readTodoList();
+  })
+  .command('removeAll', 'remove all todo', {}, () => {
+    removeAllTodo();
+  }).argv;
