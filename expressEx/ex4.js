ca2ff58b35da98fbe1617be3f20d07a8637180c4
lthong <appyegde58965@gmail.com>
2021-09-23 22:26:15 +0800

    update express ex

--- expressEx/ex4.js
+++ expressEx/ex4.js
@@ -25,10 +25,30 @@ app.get('', (req, res) => {
   res.render('index', {
     content: 'Welcome to Home page',
   });
-  // res.render('About', {
-  //   content: 'Welcome to About page',
-  // });
 });
+app.get('/about', (req, res) => {
+  // 要使用模板html可以使用 app.render(參數一, 參數二）的方法
+  // 參數一是模版的檔名，可不用帶上hbs副檔名，參數二是要傳入html的變數物件，此例中 header.hbs模板中的{{msg}}會被替換成「Welcome!」的文字秀在畫面上
+  res.render('About', {
+    content: 'Welcome to About page',
+  });
+});
+
+// express 對應路由的方式是由上往下依序找，如果在前面就找到對應路由要回應的內容後
+// 就不會再往執行其他路由相關的程式，所以如果要撰寫404頁面，
+// 則可以使用'*'這個萬用字元，這代表所有路徑都會指向此結果去做回傳
+// 所以如果前面設定的路徑都沒有對應，那就代表要回傳404頁面，然後記得要放在最後一個路由設定去做檢查
+app.get('/about/*', (req,res) => {
+  res.render(('404'),{ content: 'Not found for about page' })
+})
+app.get('*', (req,res) => {
+  res.render(('404'),{ content: 'Sorry, you have a wrong page!' })
+})
+
+// 為了要能更新.hbs相關檔案時，也能存檔後重新編譯
+// 執行此程式的指令可以使用 「nodemon ex4 -e js,hbs」
+// 如此不管是 js or hbs 檔案都可以在程式更新並存檔後自動編譯
+// 不須重新執行「nodemon ex4」，到瀏覽去重新載入就可以看到最新的結果
 app.listen('3040', () => {
-  console.log('Server is up on port 3030!');
+  console.log('Server is up on port 3040!');
 });
